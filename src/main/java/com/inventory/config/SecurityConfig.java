d2ad3a0ba9c0ca15e0422a8c5483eff18054cdb0
kavin.p <kavin.p@goomo.com>
2020-10-14 20:48:51 +0530

    token added

--- src/main/java/com/inventory/config/SecurityConfig.java
+++ src/main/java/com/inventory/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/create_user/V1.0","/login/V1.0")
+        http.csrf().disable().authorizeRequests().antMatchers("/login/V1.0")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
